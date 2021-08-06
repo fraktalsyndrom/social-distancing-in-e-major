051e0794d314cb0cd1d936b5cdd9906d431df056
Arian Jafari <arian91@hotmail.com>
2021-08-06 16:10:00 +0200

    Now using MusicTrack in main method. Implemented reading of clip duration. Implemented random interval time generation. Implemented reading of tracks from csv. Next step is to keep track of which tracks are currently playing, perhaps using Threads

--- src/com/sdiem/MusicTrack.java
+++ src/com/sdiem/MusicTrack.java
@@ -1,5 +1,6 @@
 package com.sdiem;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -12,6 +13,7 @@ public class MusicTrack
 	private boolean isPlaying;
 	private AudioInputStream inputStream;
 	private Clip clip;
+	private float duration;
 
 	public MusicTrack(String name, String path)
 	{
@@ -19,6 +21,7 @@ public class MusicTrack
 		{
 			this.name = name;
 			File file = new File(path).getAbsoluteFile();
+			duration = extractDuration(file);
 			this.inputStream  = AudioSystem.getAudioInputStream(file);
 			clip = AudioSystem.getClip();
 			clip.open(inputStream);
@@ -29,8 +32,19 @@ public class MusicTrack
 		}
 	}
 
-	public void start()
+	private float extractDuration(File file)
 	{
+		AudioFormat format = inputStream.getFormat();
+		long audioFileLength = file.length();
+		int frameSize = format.getFrameSize();
+		float frameRate = format.getFrameRate();
+
+		return (audioFileLength / (frameSize * frameRate));
+	}
+
+	public void play()
+	{
+		System.out.println("Playing track " + name);
 		clip.loop(1);
 		isPlaying = true;
 	}
---
+++ src/com/sdiem/SdiemPlayer.java
@@ -0,0 +1,69 @@
+package com.sdiem;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class SdiemPlayer
+{
+    private static final int MINIMUM_INTERVAL_SECONDS = 10;
+    private static final int MAXIMUM_INTERVAL_SECONDS = 30;
+
+    List<MusicTrack> trackList;
+
+    public SdiemPlayer(List<MusicTrack> trackList)
+    {
+        this.trackList = trackList;
+    }
+
+    public void startPlaying() throws InterruptedException
+    {
+        for (MusicTrack track : trackList)
+        {
+            track.play();
+            Thread.sleep(generateRandomIntervalTime());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            SdiemPlayer player = new SdiemPlayer(createTrackList("tracklist.csv"));
+            player.startPlaying();
+
+            Thread.sleep(TimeUnit.MINUTES.toMillis(2));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<MusicTrack> createTrackList(String trackListPath) throws IOException
+    {
+        BufferedReader csvReader = new BufferedReader(new FileReader(trackListPath));
+        List<MusicTrack> trackList = new ArrayList<>();
+        String row = "";
+        while ((row = csvReader.readLine()) != null)
+        {
+            String[] trackInfo = row.split(",");
+            MusicTrack newTrack = new MusicTrack(trackInfo[0], trackInfo[1]);
+            trackList.add(newTrack);
+        }
+        csvReader.close();
+        return trackList;
+    }
+
+    private static int generateRandomIntervalTime()
+    {
+        int range = MAXIMUM_INTERVAL_SECONDS - MINIMUM_INTERVAL_SECONDS;
+        Random random = new Random();
+        int intervalTime = random.nextInt(range) + MINIMUM_INTERVAL_SECONDS;
+        return (int) TimeUnit.SECONDS.toMillis(intervalTime);
+    }
+}
